089570df356933f969ebc0e3a18e014815392245
Oleg-Kula <kulacholeg@gmail.com>
2023-01-17 17:48:45 +0200

    SQL and README

---
+++ sql/insert_script.sql
@@ -0,0 +1,14 @@
+insert into genres(id, name)
+values
+(1, 'horror'),
+(2, 'detective'),
+(3, 'fantastic');
+
+insert into books(id, author, title, genre_id)
+VALUES
+(1, 'Test Author 1', 'Test Title 1', 3),
+(2, 'Test Author 2', 'Test Title 2', 3),
+(3, 'Test Author 3', 'Test Title 3', 3),
+(4, 'Test Author Search', 'Test Title 4', 2),
+(5, 'Test Author Search', 'Test Title 5', 2),
+(6, 'Test Author Search', 'Test Title 6', 2)
